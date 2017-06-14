33554ba0e799ba39723c8de6461469184021dacd
John McClure <mcclurejt@gmail.com>
2017-06-13 23:30:18 -0500

    Added tables and basic layout of app

--- Splash-Page/src/app/app.component.ts
+++ Splash-Page/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  date;
+  time: string;
+
+  constructor() {
+    this.date = new Date();
+    setInterval(() => {
+      this.date = new Date();
+
+    }, 1000);
+    if (this.date.getHours() >= 11) {
+      this.time = (this.date.getHours() - 12).toString()
+      this.time += ':'
+      if(this.date.getMinutes() < 10){
+        this.time += '0' + this.date.getMinutes().toString()
+      }
+      else{
+        this.time += this.date.getMinutes().toString()
+      }
+      
+    }
+    else {
+      this.time = (this.date.getHours() + 1).toString()
+      this.time += ':'
+      this.time += this.date.getMinutes()
+    }
+  }
+
+
 }
