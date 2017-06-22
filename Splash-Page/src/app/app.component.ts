d72f31fe287e4033da30b52ad819b3160b62fee4
John McClure <mcclurejt@gmail.com>
2017-06-22 13:18:47 -0500

    Minor fix

--- Splash-Page/src/app/app.component.ts
+++ Splash-Page/src/app/app.component.ts
@@ -8,30 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   date;
   time: string;
-
   constructor() {
+    // Set the time
     this.date = new Date();
     setInterval(() => {
       this.date = new Date();
-
     }, 1000);
-    if (this.date.getHours() >= 11) {
+    if (this.date.getHours() > 12) {
       this.time = (this.date.getHours() - 12).toString()
-      this.time += ':'
-      if(this.date.getMinutes() < 10){
+      this.time += ':';
+      if (this.date.getMinutes() < 10) {
         this.time += '0' + this.date.getMinutes().toString()
       }
-      else{
+      else {
         this.time += this.date.getMinutes().toString()
       }
-      
-    }
-    else {
-      this.time = (this.date.getHours() + 1).toString()
-      this.time += ':'
-      this.time += this.date.getMinutes()
+    } else {
+      this.time = (this.date.getHours()).toString()
+      this.time += ':';
+      if (this.date.getMinutes() < 10) {
+        this.time += '0' + this.date.getMinutes().toString()
+      } else {
+        this.time += this.date.getMinutes().toString()
+      }
     }
   }
+
 
 
 }
