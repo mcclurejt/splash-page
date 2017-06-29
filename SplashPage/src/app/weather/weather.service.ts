8825083743b7897dbbb8d1fcb125e3db47cd2f30
John McClure <mcclurejt@gmail.com>
2017-06-29 11:21:47 -0500

    Cleaned up weather calls

--- SplashPage/src/app/weather/weather.service.ts
+++ SplashPage/src/app/weather/weather.service.ts
@@ -6,19 +6,37 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class WeatherService {
 
-  mapsApiKey = 'AIzaSyCQyVbMdr7JFtL0lA-VCW8RmTq2o3xnGgE';
   weatherApiKey = '&APPID=dd6e2828f19f1157a6935e1e7d8f6b9e';
+  weatherBaseUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='
+  units = 'imperial';
+
   constructor(private http: Http) { }
 
-  getLocationFromCoords(latitude: number, longitude: number): Observable<any> {
-    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=` + String(latitude) + `,` + String(longitude) + `&key=` + this.mapsApiKey;
-    return this.http.get(url).map(this.extractData);
-  }
+  // getLocation(position): Observable<any> {
+  //   let latitude = position.coords.latitude;
+  //   let longitude = position.coords.longitude;
+  //   let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=` + String(latitude) + `,` + String(longitude) + `&key=` + this.mapsApiKey;
+  //   return this.http.get(url).map(this.extractData);
+  // }
 
-  getLocalWeather(zipcode: string, country: string): Observable<any> {
-    let url = 'http://api.openweathermap.org/data/2.5/weather?zip='+ zipcode + ',' + country + this.weatherApiKey;
-    console.log('url',url);
-    return this.http.get(url).map(this.extractData);
+  // getWeather(location): Observable<any> {
+  //   let zipcode = location.results[3].address_components[0].short_name;
+  //   let country = location.results[3].address_components[3].short_name;
+  //   let url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',' + country + this.weatherApiKey;
+  //   return this.http.get(url).map(this.extractData);
+  // }
+
+  load(position): Promise<any>{
+    return new Promise(resolve => {
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      let url = this.weatherBaseUrl + latitude + '&lon=' + longitude + '&units=' + this.units + this.weatherApiKey;
+      this.http.get(url)
+        .map(this.extractData)
+        .subscribe(weatherData => {
+        resolve(weatherData);
+        })
+    })
   }
 
   private extractData(res: Response) {
