5a4c9b9387fa9a68774db5f9c7df487a00d28564
Collin Moore <collin.moore96@gmail.com>
2017-07-26 17:28:58 -0500

    committing before pulling

--- SplashPage/src/app/google/gmail.service.ts
+++ SplashPage/src/app/google/gmail.service.ts
@@ -1,22 +1,47 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { GapiService } from "app/google/gapi.service";
+import { Subscription } from "rxjs/Rx";
 
 @Injectable()
 export class GmailService {
 
-  constructor() { }
+  private gapiSignedInSubscription: Subscription;
+  private nextPageToken: string;
 
-  // Email API
+
+  constructor(private gapiService: GapiService) {
+    this.gapiSignedInSubscription = this.gapiService.getIsSignedInStream()
+    .subscribe((isSignedIn: boolean) => {
+      this.getEmailMessagesList();
+    });
+  }
+
+  // getEmails(): Observable<any> {
+  //   return this.getEmailMessagesList()
+  //   .map((response) => {
+  //     this.nextPageToken = response.result.nextPageToken;
+  //     return response.result.messages;})
+  //     .flatMap((result) => {
+        
+  //      })
+  // }
 
   getEmailMessagesList(): Observable<any> {
-   return Observable.fromPromise(new Promise((resolve, reject) => {
-    gapi.client.request({
-      path: 'https://www.googleapis.com/gmail/v1/me/messsages',
-      method: 'GET',
-    }).then((response) => { 
-      resolve(response);
-    });
-   }));
+    return Observable.fromPromise(new Promise((resolve, reject) => {
+      this.gapiService.getIsSignedInStream().subscribe((isSignedIn) => {
+        if (isSignedIn) {
+
+          gapi.client.request({
+            path: 'https://www.googleapis.com/gmail/v1/users/me/messages',
+            method: 'GET',
+          }).then((response) => {
+            resolve(response);
+            // console.log("here is the messages", response);
+          });
+        }
+      });
+    }));
   }
 
 }
--- SplashPage/src/app/mail/mail.component.ts
+++ SplashPage/src/app/mail/mail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GmailService } from "app/google/gmail.service";
 
 @Component({
   selector: 'app-mail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gmailService: GmailService) {
+    
+   }
 
   ngOnInit() {
+
   }
 
 }
