ffef098dd9c5b4c9c6ec13d173b7b8f318564e36
Collin Moore <collin.moore96@gmail.com>
2017-08-01 21:55:14 -0500

    Got the message parsed. Next is generic mail service and ngrx store

--- SplashPage/src/app/content-providers/google/gmail.service.ts
+++ SplashPage/src/app/content-providers/google/gmail.service.ts
@@ -5,14 +5,15 @@ import { Subscription } from "rxjs/Rx";
 import { GoogleMessageList } from "app/models/google-message-list";
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/observable/fromPromise';
-import { GoogleMessage } from "app/models/google-message";
+import { EmailMessage } from "app/models/emailMessage";
+import * as _ from "lodash";
+
 
 @Injectable()
 export class GmailService {
 
   private gapiSignedInSubscription: Subscription;
   private nextPageToken: string;
-  private messageMap: Map<string, GoogleMessage>;
 
 
   constructor(private gapiService: GapiService) {
@@ -23,7 +24,6 @@ export class GmailService {
     console.log("getEmails()");
     return this.requestEmailIds()
     .map((response) => {
-      console.log('Email Response',response);
       return response.result })
     .flatMap((messageList) => this.getEmailsFromList(messageList))
     .map((response) => this.mapMessages(response)).share();
@@ -35,14 +35,12 @@ export class GmailService {
             path: 'https://www.googleapis.com/gmail/v1/users/me/messages',
             method: 'GET',
           }).then((response) => {
-            console.log("getEmailMessagesList()", response);
             resolve(response);
           });
         }));
   }
 
   getEmailsFromList(messageList): Observable<any> {
-    console.log("getEmailsFromList()",messageList);
     this.nextPageToken = messageList.nextPageToken;
     const messages = messageList.messages;
     let gapi = window['gapi'];
@@ -72,13 +70,78 @@ export class GmailService {
     let response = any[0];
     let messageIds = any[1];
 
-    let messageList: GoogleMessage[] = [];
-    // for (let i = 0; i<messageIds.length; i++) {
-    //   let message = response[messageIds[i].id].result;
-    //   let gMessage = Object.setPrototypeOf(message, GoogleMessage.prototype);
-    //   this.messageMap.set(messageIds[i].id, gMessage);
+    let messageList: EmailMessage[] = [];
+    for (let i = 0; i<messageIds.length; i++) {
+      let message = response[messageIds[i].id].result;
+      let email = this.mapGoogleMessageToEmailMessage(message);
+      messageList.push(email);
+    }
+    return _.keyBy(messageList, 'id');
+  }
 
-    // }
-    return response;
+  mapGoogleMessageToEmailMessage(gMessage: any): EmailMessage {
+    let result: any = {
+      id: gMessage.id,
+      threadId: gMessage.threadId,
+      labelIds: gMessage.labelIds
+    };
+    if (gMessage.internalDate) {
+      result.internalDate = parseInt(gMessage.internalDate);
+    }
+
+    if (!gMessage.payload) {
+      return new EmailMessage(result);
+    }
+
+    let headers = this.indexHeaders(gMessage.payload.headers);
+    result.headers = headers;
+
+    let parts = [gMessage.payload];
+    let firstPartProcessed = false;
+
+    while (parts.length !== 0) {
+      let part = parts.shift();
+      if (part.parts) {
+        parts = parts.concat(part.parts);
+      }
+      if (firstPartProcessed) {
+        headers = this.indexHeaders(part.headers);
+      }
+
+      let isHtml = part.mimeType && part.mimeType.indexOf('text/html') !== -1;
+      let isPlain = part.mimeType && part.mimeType.indexOf('text/plain') !== -1;
+      let isAttachment = headers['content-disposition'] && headers['content-disposition'].indexOf('attachment') !== -1;
+
+      if (isHtml && !isAttachment) {
+        result.textHtml = atob(part.body.data.replace(/-/g, '+').replace(/_/g, '/') );
+      } else if (isPlain && !isAttachment) {
+        result.textPlain = atob(part.body.data.replace(/-/g, '+').replace(/_/g, '/'));
+      } else if (isAttachment) {
+        var body = part.body;
+        if (!result.attachments) {
+          result.attachments = [];
+        }
+        result.attachments.push({
+          filename: part.filename,
+          mimeType: part.mimeType,
+          size: body.size,
+          attachmentId: body.attachmentId
+        });
+      }
+      firstPartProcessed = true;
+    }
+
+    return new EmailMessage(result);
+  }
+
+  indexHeaders(headers: any): any {
+    if (!headers) {
+      return {};
+    } else {
+      return headers.reduce((result, header) => {
+        result[header.name.toLowerCase()] = header.value;
+        return result; 
+       }, {});
+    }
   }
 }
